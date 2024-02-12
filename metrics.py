0e7d09ee87f108ab16327f78e2f22525d4e7f7fc
m4thinking <sebastianguzman@ug.uchile.cl>
2024-02-11 21:39:15 -0300

    Refactor metrics.py to calculate test accuracy and save model as ONNX

--- metrics.py
+++ metrics.py
@@ -10,18 +10,21 @@ if __name__ == '__main__':
     args, name, exp_dir, ckpt, version, dm, net = get_arguments(log_dir, "metrics")
     
     model = TrainerModule.load_from_checkpoint(checkpoint_path=ckpt, model=net)
-    # Calcular accuracy de test
-    accuracy = Accuracy(task='multiclass', num_classes=dm.num_classes)
-    net = model.model.cpu()
-    # Guardar en checkpoints como un onnx
-    print(f"Saving model from {ckpt} to {os.path.join('checkpoints', name, f'best_model_v{version}.onnx')}")
-    torch.onnx.export(net, torch.randn(1, 3, 32, 32), os.path.join("checkpoints", name, "best_model.onnx"))
-    net.to('cuda')
+    accuracy = Accuracy(task='multiclass', num_classes=dm.num_classes) # Calcular accuracy de test
+    net = model.model.to('cuda')
     net.eval()
     with torch.no_grad():
         for x, y in dm.test_dataloader():
             y_hat = net(x.to('cuda')).cpu()
             accuracy(y_hat, y)
 
-    print(f"Accuracy: {accuracy.compute()}")
+    epoch = int(ckpt.split('=')[1].split('-')[0])
+    test_accuracy = accuracy.compute() * 100
+    
+    print(f"Test accuracy: {test_accuracy}")
     print(f"Total parameters: {sum(p.numel() for p in net.parameters())/1e6:.2f}M")
+    # Guardar en checkpoints como un onnx
+    path = os.path.join('checkpoints', name, f'epoch={epoch:02d}-acc={test_accuracy:.2f}_v{version}.onnx')
+    net = net.cpu()
+    print(f"Saving model from: \n {ckpt} to {path}")
+    torch.onnx.export(net, torch.randn(1, 3, 32, 32), path)
