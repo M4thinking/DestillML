3f37e7f893366ba9f00a17b8c16d58544acfb3a2
m4thinking <sebastianguzman@ug.uchile.cl>
2024-02-23 13:52:54 -0300

    Update XAI class and add GradientExplainer

--- explainer.py
+++ explainer.py
@@ -12,6 +12,8 @@ from torchvision import transforms
 # 🎯 Shap Values
 import shap
 
+# 📚 Other libraries
+import numpy as np
 class XAI(nn.Module):
     def __init__(self, model: nn.Module) -> None:
         super().__init__()
@@ -84,89 +86,95 @@ class XAI(nn.Module):
     
     def shap(self, X, to_explain, class_names, inv_normalize = None, true_labels = None):
         # X: (N1, 3, 224, 224) # to_explain: (N2, 3, 224, 224)
-        explainer = shap.DeepExplainer(self.model, X)
-        shap_values, indexes = explainer.shap_values(to_explain, ranked_outputs=2)
-        index_names = np.vectorize(lambda x: class_names[str(x)][1])(indexes)
+        # explainer = shap.DeepExplainer(self.model, X)
+        # shap_values, indexes = explainer.shap_values(to_explain, ranked_outputs=2)
+        
+        explainer = shap.GradientExplainer((self.model, self.model.layer4), X)
+        shap_values, indexes = explainer.shap_values(to_explain, ranked_outputs=2, nsamples=500)
+        
+        index_names = np.vectorize(lambda x: class_names[x])(indexes)
         shap_values = [np.swapaxes(np.swapaxes(s, 2, 3), 1, -1) for s in shap_values]
+        print(len(shap_values), shap_values[0].shape)
         # plot the feature attributions in numpy image format (H, W, C)
         to_explain = inv_normalize(to_explain).numpy()
         to_explain = np.swapaxes(np.swapaxes(to_explain, 1, -1), 1, 2)
-        shap.image_plot(shap_values, to_explain, index_names, true_labels)
+        shap.image_plot(shap_values, to_explain, index_names, True, true_labels)
     
     
 if __name__ == "__main__":
-    # 🧠 Models
-    from VGG import vgg19_bn
-    from PIL import Image
+    pass
+    # # 🧠 Models
+    # from VGG import vgg19_bn
+    # from PIL import Image
     
-    ckpt_path = "checkpoints/vgg19/vgg19_bn.pth"
-    teacher = vgg19_bn(pretrained=False, progress=True, num_classes=1000, init_weights=True, dropout=0.5)
-    teacher.load_state_dict(torch.load(ckpt_path))
-    teacher.eval()
+    # ckpt_path = "checkpoints/vgg19/vgg19_bn.pth"
+    # teacher = vgg19_bn(pretrained=False, progress=True, num_classes=1000, init_weights=True, dropout=0.5)
+    # teacher.load_state_dict(torch.load(ckpt_path))
+    # teacher.eval()
     
-    # {0: 'tench, Tinca tinca', 1: 'goldfish, Carassius auratus', ...}
-    with open("./data/imagenet_classes.txt") as f:
-        labels = [line.strip().split(":")[-1].strip() for line in f.readlines()]
+    # # {0: 'tench, Tinca tinca', 1: 'goldfish, Carassius auratus', ...}
+    # with open("./data/imagenet_classes.txt") as f:
+    #     labels = [line.strip().split(":")[-1].strip() for line in f.readlines()]
     
-    img_path1 = "data/dog1.png"
-    img_path2 = "data/dog2.png"
-    true_labels = ['basenji', 'golden retriever']
-    img1 = Image.open(img_path1).convert("RGB")
-    img2 = Image.open(img_path2).convert("RGB")
+    # img_path1 = "data/dog1.png"
+    # img_path2 = "data/dog2.png"
+    # true_labels = ['basenji', 'golden retriever']
+    # img1 = Image.open(img_path1).convert("RGB")
+    # img2 = Image.open(img_path2).convert("RGB")
 
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ])
+    # transform = transforms.Compose([
+    #     transforms.ToTensor(),
+    #     transforms.Resize(256),
+    #     transforms.CenterCrop(224),
+    #     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    # ])
     
-    inv_normalize = transforms.Normalize(
-        mean=[-0.485/0.229, -0.456/0.224, -0.406/0.225],
-        std=[1/0.229, 1/0.224, 1/0.225]
-    )
+    # inv_normalize = transforms.Normalize(
+    #     mean=[-0.485/0.229, -0.456/0.224, -0.406/0.225],
+    #     std=[1/0.229, 1/0.224, 1/0.225]
+    # )
     
-    imgs = torch.stack([transform(img1), transform(img2)])
-    preds = teacher(imgs)
-    explainer = XAI(teacher)
-    heatmaps, preds = explainer.grad_cam(imgs)
+    # imgs = torch.stack([transform(img1), transform(img2)])
+    # preds = teacher(imgs)
+    # explainer = XAI(teacher)
+    # heatmaps, preds = explainer.grad_cam(imgs)
         
-    import matplotlib.pyplot as plt
-    import numpy as np
-    import cv2 as cv
+    # import matplotlib.pyplot as plt
+    # import numpy as np
+    # import cv2 as cv
     
-    imgs = inv_normalize(imgs)
+    # imgs = inv_normalize(imgs)
     
-    fig, ax = plt.subplots(2, 2, figsize=(10, 5))
-    for i in range(2):
-        img = imgs[i].permute(1, 2, 0).detach().numpy()
-        heatmap = heatmaps[i].detach().numpy()
-        heatmap = cv.resize(heatmap, (img.shape[1], img.shape[0]))
-        heatmap = np.uint8(255 * heatmap)
-        heatmap = cv.applyColorMap(heatmap, cv.COLORMAP_JET)
-        superimposed_img = heatmap * 0.5 + img * 255
-        superimposed_img = superimposed_img / np.max(superimposed_img)
-        ax[0, i].imshow(img)
-        ax[1, i].imshow(superimposed_img)
-        ax[0, i].axis("off")
-        ax[1, i].axis("off")
-        ax[0, i].set_title(f"True label: {true_labels[i]}")
-        ax[1, i].set_title(f"Predicted label: {labels[preds[i]]}")
+    # fig, ax = plt.subplots(2, 2, figsize=(10, 5))
+    # for i in range(2):
+    #     img = imgs[i].permute(1, 2, 0).detach().numpy()
+    #     heatmap = heatmaps[i].detach().numpy()
+    #     heatmap = cv.resize(heatmap, (img.shape[1], img.shape[0]))
+    #     heatmap = np.uint8(255 * heatmap)
+    #     heatmap = cv.applyColorMap(heatmap, cv.COLORMAP_JET)
+    #     superimposed_img = heatmap * 0.5 + img * 255
+    #     superimposed_img = superimposed_img / np.max(superimposed_img)
+    #     ax[0, i].imshow(img)
+    #     ax[1, i].imshow(superimposed_img)
+    #     ax[0, i].axis("off")
+    #     ax[1, i].axis("off")
+    #     ax[0, i].set_title(f"True label: {true_labels[i]}")
+    #     ax[1, i].set_title(f"Predicted label: {labels[preds[i]]}")
          
-    plt.tight_layout()
-    plt.show()
+    # plt.tight_layout()
+    # plt.show()
 
-    # SHAP
-    import json
-    url = "https://s3.amazonaws.com/deep-learning-models/image-models/imagenet_class_index.json"
-    fname = shap.datasets.cache(url)
-    with open(fname) as f:
-        class_names = json.load(f)
+    # # SHAP
+    # import json
+    # url = "https://s3.amazonaws.com/deep-learning-models/image-models/imagenet_class_index.json"
+    # fname = shap.datasets.cache(url)
+    # with open(fname) as f:
+    #     class_names = json.load(f)
     
-    from datasets import ImageNet
-    dataset = ImageNet(root="./data/imagenet/", split="train", transform=transform)
-    X, _ = dataset[0:50]
-    to_explain, y = dataset[60:62]
-    true_labels = [class_names[str(y[i].item())][1] for i in range(y.shape[0])]
-    explainer.shap(X, to_explain, class_names, inv_normalize, true_labels)
+    # from datasets import ImageNet
+    # dataset = ImageNet(root="./data/imagenet/", split="train", transform=transform)
+    # X, _ = dataset[0:50]
+    # to_explain, y = dataset[60:62]
+    # true_labels = [class_names[str(y[i].item())][1] for i in range(y.shape[0])]
+    # explainer.shap(X, to_explain, class_names, inv_normalize, true_labels)
     
